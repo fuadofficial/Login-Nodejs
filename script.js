0106d7f61cea59e34c12969bda42aed97dac9944
fuad <ahmadufuadoorakam@gmail.com>
2024-06-12 06:29:32 +0530

    created node js codes

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('views'))
+
+const emailDb = "ahmadfuadoorakam@gmail.com";
+const passwordDb = "1234";
+
+
+app.post('/login', (req, res) => {
+
+    const { email, password } = req.body;
+    if (emailDb === email && passwordDb === password) {
+        res.send('Login Successful')
+    } else {
+        res.send('Login Failed')
+    }
+})
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log("Surver Running...."))
